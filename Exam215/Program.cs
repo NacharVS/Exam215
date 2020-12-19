4479cdb3914351cfabd4880e10456b2cb492ab20
r3dinc <r3dinc@yandex.ru>
2020-12-19 10:39:39 +0300

    master fix

--- Exam215/Program.cs
+++ Exam215/Program.cs
@@ -6,7 +6,7 @@ namespace Exam215
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Leave hope behind who enters here...");
         }
     }
 }
