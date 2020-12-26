0a187f449f04f2e3b27c941e69c32024c34962cd
AdelZiFarov <adelzifarov6@gmail.com>
2020-12-26 10:43:47 +0300

    add

--- Exam215/Program.cs
+++ Exam215/Program.cs
@@ -6,7 +6,7 @@ namespace Exam215
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Leave hope behind who enters here...");
+            Console.WriteLine("Спаси и сохрани");
         }
     }
 }
