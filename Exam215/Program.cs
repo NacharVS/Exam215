2be5723dd940d52a094d99aa83824d12c3147e8a
ilfirshavaliev <naodnoyvolne@bk.ru>
2020-12-28 15:49:58 +0300

    Finish

--- Exam215/Program.cs
+++ Exam215/Program.cs
@@ -6,7 +6,29 @@ namespace Exam215
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("///");
+            Random rnd = new Random();
+            int[,] mus = new int[10, 10]; 
+            int a = 0;
+            Console.WriteLine("Введите число");
+            int b = int.Parse(Console.ReadLine());
+            for (int i = 0; i < 10; i++)
+            { 
+                for (int j = 0; j < 10; j++) 
+                {
+                    mus[i, j] = rnd.Next(1, 10); 
+                    Console.Write(mus[i, j] + " ");
+                    if (mus[i,j] == b)
+                    {
+                        a += 1;
+                    }
+                }
+                if (a>=0)
+                {
+                    Console.Write($"Кол-во заданного числа в строке {a}");
+                }
+                a = 0;
+                Console.WriteLine();
+            }
         }
     }
 }
