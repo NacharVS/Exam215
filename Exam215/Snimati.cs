d939ce45fc62f44ad3f09612b0707a0f98d20180
Bulatibr <ibra.bulat@yandex.ru>
2020-12-28 14:22:54 +0300

    jgjg

---
+++ Exam215/Snimati.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exam215
+{
+    interface ISnimati
+    {
+        
+        public void snimat()
+        {
+            Console.WriteLine("Введети скооко снять ");
+            int a = int.Parse(Console.ReadLine());
+
+            NewBank1.vklad = (NewBank1.vklad - a);
+
+
+        }
+
+    }
+}
---
+++ Exam215/Vevesti.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exam215
+{
+    interface IVevesti
+    {
+        public void vevesti()
+        {
+             Console.WriteLine("ВВедите сколько пополнить  ");
+            int a = int.Parse(Console.ReadLine());
+
+            NewBank1.vklad = (NewBank1.vklad + a);
+
+       }
+    }
+}
